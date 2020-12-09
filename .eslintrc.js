be4323075e747dae3a87c46bced867a5370f049b
hetyo525 <hetyoinfo@gmail.com>
2020-12-10 03:30:27 +0900

    inital commit

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    es6: true,
+  },
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'prettier/@typescript-eslint',
+  ],
+}
